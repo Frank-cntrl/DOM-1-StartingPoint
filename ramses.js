72410a1e8a521e392516c3398021a8c53f243123
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-18 15:50:18 -0400

    testing

--- ramses.js
+++ ramses.js
@@ -1,3 +1,30 @@
+//const { createElement } = require("react");
+
+console.log("hello from JS");
+
+const root = document.getElementById("root");
+const h2 = document.createElement("h2");
+const links = [
+  { text: "Homepage", url: "bonus.html" },
+  { text: "Ramses", url: "ramses.html" },
+  { text: "Frank", url: "frank.html" },
+  { text: "Michelangelo", url: "mick.html" }
+]
+
+links.forEach(({ text, url }) => {
+  const a = document.createElement("a");
+  a.href = url;
+  a.textContent = text;
+  a.style.display = "block";
+  a.style.display = "inline-block" 
+  a.style.marginRight = "40px"
+  root.appendChild(a);
+
+});
+
+
+
+
 // Simple JavaScript code to display a message on the Ramses Hobbies page
 console.log("This is Ramses Hobbies Page");
 
