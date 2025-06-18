4b472aa8be2085b1d25ad894e7da539da6ec6cab
Franccesco Petta <franccescopettamuller@gmail.com>
2025-06-18 15:49:05 -0400

    save progress

---
+++ frank.js
@@ -0,0 +1,7 @@
+const root = document.getElementById("root");
+
+const h1 = document.createElement("h1");
+h1.innerText = "This is Frank's Page";
+h1.style.fontFamily = "Georgia, serif"
+root.appendChild(h1);
+
