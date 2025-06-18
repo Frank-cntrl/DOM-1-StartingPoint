36c65422f8a1fca6ef62c0cce0298a4a7e6d7218
Franccesco Petta <franccescopettamuller@gmail.com>
2025-06-18 15:30:47 -0400

    Formatted

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log("Hello! If you see this, the script is working.");
 - [ ] Select the section with an id of container using querySelector.
 - [ ] Select all of the list items with a class of "second".
 - [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
-/*
+
 - [ ] Give the section with an id of container the text "Hello!".
 - [ ] Add the class main to the div with a class of footer.
 - [ ] Remove the class main on the div with a class of footer.
@@ -46,29 +46,55 @@ console.log("Problem 4");
 const thirdOlItem = document.querySelector("ol .third");
 console.log("thirdOlItem", thirdOlItem)
 
-//Frank Functions
 //- [ ] Give the section with an id of container the text "Hello!"
+console.log("Problem 5");
 newP = document.createElement('p');
 newP.innerText = "hello";
 containerByID = document.getElementById("container");
 containerByID.appendChild(newP);
+console.log("Hello", newP);
+
 //- [ ] Add the class main to the div with a class of footer.
+console.log("Problem 6");
 footerDiv = document.querySelector(".footer");
 footerDiv.classList.add("main");
+console.log("Class main", footerDiv.classList);
 //- [ ] Remove the class main on the div with a class of footer.
+console.log("problem 7");
 footerDiv.classList.remove("main");
+console.log("probelm 7", footerDiv.classList);
 //- [ ] Create a new li element.
+console.log('problem 8');
 newLi = document.createElement("li");
+console.log("new list", newLi);
+
 
 //- [ ] Give the li the text "four".
+console.log('problem 9');
 newLi.innerText = "four";
+console.log('New Li Text', newLi.innerText)
 //- [ ] Append the li to the ul element.
+console.log('problem 10');
 ulElement = document.querySelector('ul');
 ulElement.appendChild(newLi);
+console.log('new element', ulElement, newLi)
 //- [ ] Loop over all of the lis inside the ol tag and give them a background color of "green".
+console.log('problem 11');
 olLis = document.querySelectorAll("ol li");
 olLis.forEach(li => {
     li.style.backgroundColor = "green";
 });
+console.log('change the background color:', 'green');
 //- [ ] Remove the div with a class of footer.
+console.log('Problem 12');
+
 footerDiv.remove();
+
+function check(item){
+    if(item){
+        return true;
+    }
+return false;
+}
+
+console.log('removed class of footer', check(footerDiv));
